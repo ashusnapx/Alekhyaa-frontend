f1af85a44c4c229b17e987f8d6bf8b36832960e2
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2023-09-20 17:02:10 +0530

    sih project

--- src/Components/LoadingBar.js
+++ src/Components/LoadingBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./LoadingBar.css";
 
-function LoadingBar({progress}) {
+const LoadingBar = ({ progress }) => {
   return (
-  <>
-  <div className="loading-bar" >{progress}</div>
-  
-  </>);
-}
+    <div className="loading-bar" style={{width:"70%",height:"50px" ,margin:"0px auto",backgroundColor:"white"}}>
+      <div className="bar" style={{ width: progress + '%'}}>
+      <span className="progress-text" style={{color:"black", marginLeft:"30px"}}>{progress}%</span>
+      </div>
+    </div>
+  );
+};
 
 export default LoadingBar;
